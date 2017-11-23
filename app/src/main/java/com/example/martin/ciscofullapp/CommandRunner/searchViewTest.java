56e6d5dd89eb09b1328592fa3dde4129d0735d60
DESKTOP-LPOG585\Frederik <fstjernholm@gmail.com>
2017-11-23 11:39:47 +0100

    Merge branch 'master' of https://github.com/JustVerner/CiscoFullApp
    # Conflicts:
    #	app/src/main/java/com/example/martin/ciscofullapp/CommandRunner/CommandClass.java

--- app/src/main/java/com/example/martin/ciscofullapp/CommandRunner/searchViewTest.java
+++ app/src/main/java/com/example/martin/ciscofullapp/CommandRunner/searchViewTest.java
@@ -108,7 +108,7 @@ public class searchViewTest extends AppCompatActivity{
                 startActivity(new Intent(searchViewTest.this, CommandRunner.class).putExtra("name1", s));
                 startActivity(intent);
 
-                if(s == "Ping")
+                if(s == "ping")
                 {
                     showText = true;
                 }
